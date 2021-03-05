1e96fb1b8ed600390a69557d72212fa43fe2d64a
Gareth <riceg23@up.edu>
2021-03-05 00:52:46 -0800

    Add GameState class and onClick method in MainActivity

---
+++ app/src/main/java/com/example/gamestate/GameState.java
@@ -0,0 +1,21 @@
+package com.example.gamestate;
+
+/**
+ * @author Gareth Rice
+ *
+ * @version 3/21
+ *
+ * Notes:
+ *
+ */
+
+public class GameState {
+
+    /**
+     * ctor
+     *
+     */
+    public GameState(){
+        //Should initialize all the variables to have start of game values
+    }
+}
--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gamestate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * @author Gareth Rice
@@ -17,8 +20,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     /**
      * onCreate: setup the project
      *
@@ -28,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText mainText = (EditText) findViewById(R.id.mainText);
+
+        //Setup runTestBtn and it's onClick method
+        Button runTestBtn = (Button) findViewById(R.id.runTestButton);
+        runTestBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //clear the edit text
+                mainText.setText("");
+
+                GameState firstInstance = new GameState();
+            }
+        });
     }
+
+
 }
