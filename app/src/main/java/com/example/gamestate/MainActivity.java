17a7eb300b6b36f870975ed78ffdaf8a1e27416f
Gareth <riceg23@up.edu>
2021-03-05 00:37:46 -0800

    Add multiline editText and "Run Test" button.

--- app/src/main/java/com/example/gamestate/MainActivity.java
+++ app/src/main/java/com/example/gamestate/MainActivity.java
@@ -4,8 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author Gareth Rice
+ *
+ * @version 3/21
+ *
+ * Notes:
+ *
+ * ToDo:
+ *
+ */
+
 public class MainActivity extends AppCompatActivity {
 
+
+
+    /**
+     * onCreate: setup the project
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
