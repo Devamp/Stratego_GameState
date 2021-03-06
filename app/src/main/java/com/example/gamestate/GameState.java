ac8e5de83f03c050ed3a078bde161c94544cdcd0
Caden <deutsche23@up.edu>
2021-03-06 14:23:31 -0800

    Added Move method to GameState

--- app/src/main/java/com/example/gamestate/GameState.java
+++ app/src/main/java/com/example/gamestate/GameState.java
@@ -11,6 +11,7 @@ package com.example.gamestate;
 
 public class GameState {
     //Number of characters of each color
+    //0 - spy, 10 - bomb, 11 - flag
     private int[] blueCharacter;
     private int[] redCharacter;
     //turn indicator
@@ -64,6 +65,106 @@ public class GameState {
 
     public GameState(GameState original){
 
-
     }
+    //Check initial spot
+
+    //Check if is movable
+    public int[][] move(int sCol, int sRow, int fCol, int fRow){
+        if(board[sCol][sRow]%12 > 9  || board[sCol][sRow]%12 <0 ){
+            //INVALID
+            return null;
+        }
+        //Check if is your piece
+        else if(board[sCol][sRow] > 11 && turn == 0 || board[sCol][sRow] < 12 && turn == 1){
+            //INVALID
+            return null;
+        }
+        //Check if on board
+        else if(sCol > 9 || sRow > 9 || fCol > 9 || fRow > 9 || sCol < 0 || sRow < 0 || fCol < 0 || fRow < 0){
+            //INVALID
+            return null;
+        }
+
+        //Check final spot
+
+        //if not scout
+        if(board[sCol][sRow]%12  != 9){
+            //Check to make sure only one spot away
+            if(Math.abs(sCol - fCol) != 1 || Math.abs(sRow - fRow) != 1){
+                //INVALID
+                return null;
+            }
+            //Check to make sure not diagonal
+            else if(sCol != fCol && sRow != fRow){
+                //INVALID
+                return null;
+            }
+            //Check if empty
+            else if(board[fCol][fRow] != -1 ){
+                //INVALID
+                return null;
+            }
+        }
+        //if scout
+        else{
+            //Check to make sure not diagonal
+            if(sCol != fCol && sRow != fRow){
+                //INVALID
+                return null;
+            }
+            //Check if empty
+            else if(board[fCol][fRow] != -1 ){
+                //INVALID
+                return null;
+            }
+
+            //Check in between spots
+            if(fCol > sCol){
+                for(int i = sCol + 1; i <= fCol; i++) {
+                    if(board[i][sRow] != -1){
+                        //Invalid
+                        return null;
+                    }
+                }
+
+            }
+            else if(fCol < sCol){
+                for(int i = sCol -1; i >= fCol; i--){
+                    if(board[i][sRow] != -1){
+                        //Invalid
+                        return null;
+                    }
+
+                }
+
+            }
+            else if(fRow < sRow){
+                for(int i = sRow - 1; i >= fRow; i--){
+                    if(board[sCol][i] != -1){
+                        //Invalid
+                        return null;
+                    }
+
+                }
+
+            }
+            else if( fRow > sRow){
+                for(int i = sRow + 1; i <= fRow; i++){
+                    if(board[sCol][i] != -1){
+                        //Invalid
+                        return null;
+                    }
+                }
+            }
+        }
+
+        //Make Move
+       int temp =  board[sCol][sRow];
+        board[sCol][sRow] = -1;
+        board[fCol][fRow] = temp;
+
+        return board;
+    }
+
+
 }
